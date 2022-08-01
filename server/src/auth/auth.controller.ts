80c98732b87165b6f56308f90063788da2fc1876
Ivan Vukman <ivan.vukman@gmail.com>
2022-08-01 12:10:12 +0200

    Fix lint errors

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,12 +1,8 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { ReadUserDto } from 'src/users/dto/read-user.dto';
 import { AuthService } from './auth.service';
-import { BuyerGuard } from './buyer.guard';
 import { AuthCredentialsDto } from './dto/auth.credentials.dto';
-import { GetUser } from './get-user.decorator';
-import { JwtPayload } from './jwt-payload.interface';
 
 @Controller('auth')
 export class AuthController {
